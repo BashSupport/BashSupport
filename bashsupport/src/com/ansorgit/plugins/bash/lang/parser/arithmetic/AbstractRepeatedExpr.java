4ce1bdda0ef069ddf65e8be7f9533f3065bae9b5
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2010-02-06 17:46:57 +0000

    second take on the arithmetic expression parsing

---
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/parser/arithmetic/AbstractRepeatedExpr.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright 2009 Joachim Ansorg, mail@ansorg-it.com
+ * File: AbstractRepeatedExpr.java, Class: AbstractRepeatedExpr
+ * Last modified: 2010-02-06
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.ansorgit.plugins.bash.lang.parser.arithmetic;
+
+import com.ansorgit.plugins.bash.lang.parser.BashPsiBuilder;
+import com.ansorgit.plugins.bash.lang.parser.ParsingFunction;
+import com.ansorgit.plugins.bash.lang.parser.util.ParserUtil;
+import com.intellij.lang.PsiBuilder;
+import com.intellij.psi.tree.IElementType;
+import com.intellij.psi.tree.TokenSet;
+
+/**
+ * Abstract class for a repeated expression which delegates the parsing of the subexpressions to
+ * another parsing function.
+ * <p/>
+ * User: jansorg
+ * Date: Feb 6, 2010
+ * Time: 5:22:23 PM
+ */
+abstract class AbstractRepeatedExpr implements ParsingFunction {
+    private final ParsingFunction next;
+    private final TokenSet operators;
+    private final IElementType partMarker;
+
+    protected AbstractRepeatedExpr(ParsingFunction next, TokenSet operators, IElementType partMarker) {
+        this.next = next;
+        this.operators = operators;
+        this.partMarker = partMarker;
+    }
+
+    protected AbstractRepeatedExpr(ParsingFunction next, TokenSet operators) {
+        this(next, operators, null);
+    }
+
+    protected AbstractRepeatedExpr(ParsingFunction next, IElementType operator) {
+        this(next, TokenSet.create(operator), null);
+    }
+
+    public boolean isValid(IElementType token) {
+        throw new IllegalStateException("unsupported");
+    }
+
+    public boolean isValid(BashPsiBuilder builder) {
+        return next.isValid(builder);
+    }
+
+    public boolean parse(BashPsiBuilder builder) {
+        PsiBuilder.Marker marker = builder.mark();
+
+        boolean ok = true;
+        do {
+            ok = next.parse(builder);
+        } while (ok && ParserUtil.conditionalRead(builder, operators));
+
+        if (ok && partMarker != null) {
+            marker.done(partMarker);
+        } else {
+            marker.drop();
+        }
+
+        return ok;
+    }
+}
