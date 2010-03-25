d74447ad8b72339424b44a956d0f9ff22d5ed2b7
J. Ansorg <github@joachim-ansorg.de>
2010-03-25 19:13:52 +0000

    fixes

--- src/com/ansorgit/plugins/bash/actions/repl/AbstractBashReplAction.java
+++ src/com/ansorgit/plugins/bash/actions/repl/AbstractBashReplAction.java
@@ -1,7 +1,7 @@
 /*
- * Copyright 2009 Joachim Ansorg, mail@ansorg-it.com
+ * Copyright 2010 Joachim Ansorg, mail@ansorg-it.com
  * File: AbstractBashReplAction.java, Class: AbstractBashReplAction
- * Last modified: 2010-03-04
+ * Last modified: 2010-03-25
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -80,8 +80,6 @@ abstract class AbstractBashReplAction extends AnAction {
                         "evaluate.norepl.message",
                         "evaluate.norepl.title");
             }
-
-            return;
         }
 
         /*if (ClojurePsiFactory.getInstance(project).hasSyntacticalErrors(textToEvaluate)) {
