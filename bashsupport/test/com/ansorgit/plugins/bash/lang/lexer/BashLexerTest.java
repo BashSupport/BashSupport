b79cc6bacb81d885f502be65e4f8721c0ca11392
J. Ansorg <github@joachim-ansorg.de>
2010-04-26 16:42:50 +0000

    fixed compilation for Mac/Java5, fixed unknown escape characters

--- bashsupport/test/com/ansorgit/plugins/bash/lang/lexer/BashLexerTest.java
+++ bashsupport/test/com/ansorgit/plugins/bash/lang/lexer/BashLexerTest.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 Joachim Ansorg, mail@ansorg-it.com
  * File: BashLexerTest.java, Class: BashLexerTest
- * Last modified: 2010-04-24
+ * Last modified: 2010-04-26
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -224,6 +224,21 @@ public class BashLexerTest {
         //test escaped chars
         testTokenization("\\*", WORD);
         testTokenization("\\ ", WORD);
+        testTokenization("\\{", WORD);
+        testTokenization("\\;", WORD);
+        testTokenization("\\.", WORD);
+        testTokenization("\\r", WORD);
+        testTokenization("\\n", WORD);
+        testTokenization("\\:", WORD);
+        testTokenization("\\(", WORD);
+        testTokenization("\\)", WORD);
+        testTokenization("\\\"", WORD);
+        testTokenization("\\\\", WORD);
+        testTokenization("\\>", WORD);
+        testTokenization("\\<", WORD);
+        testTokenization("\\$", WORD);
+        testTokenization("\\ ", WORD);
+        testTokenization("\\?", WORD);
         testTokenization("\\!", WORD);
 
         //subshells
