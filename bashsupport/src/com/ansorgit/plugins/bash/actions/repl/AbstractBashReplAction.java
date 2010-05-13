b94240bb62f8727ba7ceee027f1a57630fac5ee9
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2010-05-13 00:45:42 +0000

    removed files

--- bashsupport/src/com/ansorgit/plugins/bash/actions/repl/AbstractBashReplAction.java
+++ bashsupport/src/com/ansorgit/plugins/bash/actions/repl/AbstractBashReplAction.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 Joachim Ansorg, mail@ansorg-it.com
  * File: AbstractBashReplAction.java, Class: AbstractBashReplAction
- * Last modified: 2010-04-24
+ * Last modified: 2010-05-13
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -18,21 +18,15 @@
 
 package com.ansorgit.plugins.bash.actions.repl;
 
-import com.ansorgit.plugins.bash.repl.ReplManager;
-import com.ansorgit.plugins.bash.repl.ReplPanel;
 import com.ansorgit.plugins.bash.settings.facet.BashFacet;
 import com.ansorgit.plugins.bash.settings.facet.BashFacetType;
 import com.intellij.facet.FacetManager;
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.LangDataKeys;
-import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.module.Module;
 import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
-import com.intellij.openapi.ui.Messages;
-import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 
 abstract class AbstractBashReplAction extends AnAction {
 
@@ -62,58 +56,4 @@ abstract class AbstractBashReplAction extends AnAction {
         }
         return module;
     }
-
-    static void evaluateInCurrentRepl(@NotNull String textToEvaluate, @NotNull AnActionEvent event) {
-        evaluateInCurrentRepl(textToEvaluate, event, true, false);
-    }
-
-    static void evaluateInCurrentRepl(@NotNull String textToEvaluate, @NotNull AnActionEvent event, boolean showErrors, boolean requestFocus) {
-        Project project = event.getData(LangDataKeys.PROJECT);
-        if (project == null) {
-            return;
-        }
-
-        ReplPanel repl = getCurrentRepl(event);
-        if (repl == null) {
-            if (showErrors) {
-                Messages.showErrorDialog(project,
-                        "evaluate.norepl.message",
-                        "evaluate.norepl.title");
-            }
-        }
-
-        /*if (ClojurePsiFactory.getInstance(project).hasSyntacticalErrors(textToEvaluate)) {
-         if (showErrors) {
-           Messages.showErrorDialog(project,
-                 ClojureBundle.message("evaluate.incorrect.sexp"),
-                 ClojureBundle.message("evaluate.incorrect.cannot.evaluate"));
-         }
-         return;
-       } */
-
-        //repl.writeToCurrentRepl(textToEvaluate, requestFocus);
-    }
-
-
-    @Nullable
-    static ReplManager getReplManager(final AnActionEvent e) {
-        final Project project = PlatformDataKeys.PROJECT.getData(e.getDataContext());
-        return project == null ? null : ReplManager.getInstance(project);
-    }
-
-    @Nullable
-    static ReplPanel getCurrentRepl(final AnActionEvent e) {
-        final ReplManager replManager = getReplManager(e);
-        if (replManager != null) {
-            return replManager.getCurrentRepl();
-        }
-
-        return null;
-    }
-
-    //  protected @Nullable String getFilePath(@NotNull AnActionEvent e) {
-    //    VirtualFile vfile = e.getData(DataKeys.VIRTUAL_FILE);
-    //    return vfile != null ? vfile.getPath() : null;
-    //  }
-
 }
