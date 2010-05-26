9c1cebdeb5f4f346b52abb09c9388a240efb9a58
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2010-05-26 20:36:40 +0000

    Fixed missing repl dir

--- bashsupport/src/com/ansorgit/plugins/bash/actions/AddReplAction.java
+++ bashsupport/src/com/ansorgit/plugins/bash/actions/AddReplAction.java
@@ -1,7 +1,7 @@
 /*
  * Copyright 2010 Joachim Ansorg, mail@ansorg-it.com
  * File: AddReplAction.java, Class: AddReplAction
- * Last modified: 2010-05-13
+ * Last modified: 2010-05-26
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,16 +16,23 @@
  * limitations under the License.
  */
 
-package com.ansorgit.plugins.bash.actions.repl;
+package com.ansorgit.plugins.bash.actions;
 
 import com.ansorgit.plugins.bash.runner.repl.BashConsoleRunner;
+import com.ansorgit.plugins.bash.settings.facet.BashFacet;
+import com.ansorgit.plugins.bash.settings.facet.BashFacetType;
 import com.ansorgit.plugins.bash.util.BashIcons;
+import com.intellij.facet.FacetManager;
+import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.actionSystem.LangDataKeys;
 import com.intellij.openapi.actionSystem.Presentation;
 import com.intellij.openapi.module.Module;
+import com.intellij.openapi.module.ModuleManager;
 import com.intellij.openapi.project.Project;
+import org.jetbrains.annotations.Nullable;
 
-public class AddReplAction extends AbstractBashReplAction {
+public class AddReplAction extends AnAction {
     private static final com.intellij.openapi.diagnostic.Logger log = com.intellij.openapi.diagnostic.Logger.getInstance("AddReplAction");
 
     public AddReplAction() {
@@ -59,4 +66,31 @@ public class AddReplAction extends AbstractBashReplAction {
         }
     }
 
+    @Nullable
+    static Module getModule(AnActionEvent e) {
+        Module module = e.getData(LangDataKeys.MODULE);
+        if (module == null) {
+            final Project project = e.getData(LangDataKeys.PROJECT);
+            if (project == null) {
+                return null;
+            }
+            final Module[] modules = ModuleManager.getInstance(project).getModules();
+            if (modules.length == 1) {
+                module = modules[0];
+            } else {
+                for (Module m : modules) {
+                    final FacetManager manager = FacetManager.getInstance(m);
+                    final BashFacet clFacet = manager.getFacetByType(BashFacetType.INSTANCE.getId());
+                    if (clFacet != null) {
+                        module = m;
+                        break;
+                    }
+                }
+                if (module == null) {
+                    module = modules[0];
+                }
+            }
+        }
+        return module;
+    }
 }
