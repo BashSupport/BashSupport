926a7d1d683c3ef648c89513d013cafd57795d1a
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2011-01-26 20:19:11 +0000

    Incorrect inspection of unused parameter when using $* inside function

--- bashsupport/src/com/ansorgit/plugins/bash/lang/LanguageBuiltins.java
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/LanguageBuiltins.java
@@ -38,7 +38,7 @@ public final class LanguageBuiltins {
     //variables which are references to parameters. This is just a reasonable default set, Bash doesn't seem to restrict
     //the range of valid parameter
     public static final Collection<String> bashShellParamReferences = Sets.newHashSet(
-            "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"
+            "*", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19"
     );
 
     /**
--- bashsupport/testData/psi/inspection/unusedFunctionParameter/ok/src/A.bash
+++ bashsupport/testData/psi/inspection/unusedFunctionParameter/ok/src/A.bash
@@ -4,4 +4,9 @@ function a {
     echo a $1 1>&2 $2 ${3}
 }
 
+function x {
+    echo $*
+}
+
 a a b c
+x a
