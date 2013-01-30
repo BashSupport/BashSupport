25d08b4c1777e76869830e3cfec7b339b3937478
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2013-01-30 23:16:26 +0000

    - Changed file template to use /bin/bash instead of /bin/sh

--- bashsupport/src/com/ansorgit/plugins/bash/actions/BashTemplatesFactory.java
+++ bashsupport/src/com/ansorgit/plugins/bash/actions/BashTemplatesFactory.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
- * Copyright 2011 Joachim Ansorg, mail@ansorg-it.com
+/*
+ * Copyright 2010 Joachim Ansorg, mail@ansorg-it.com
  * File: BashTemplatesFactory.java, Class: BashTemplatesFactory
- * Last modified: 2011-04-30 16:33
+ * Last modified: 2013-01-31
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package com.ansorgit.plugins.bash.actions;
 
@@ -54,7 +54,7 @@ public class BashTemplatesFactory implements FileTemplateGroupDescriptorFactory 
     public static PsiFile createFromTemplate(final PsiDirectory directory, final String name, String fileName) throws IncorrectOperationException {
         log.debug("createFromTemplate: dir:" + directory + ", filename: " + fileName);
 
-        final String text = "#!/bin/sh\n";
+        final String text = "#!/bin/bash\n";
         final PsiFileFactory factory = PsiFileFactory.getInstance(directory.getProject());
 
         log.debug("Create file from text");
