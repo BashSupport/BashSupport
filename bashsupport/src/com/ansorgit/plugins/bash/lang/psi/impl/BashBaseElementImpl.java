4eb82277a984a79badeb1f5268fe54d95c7495ec
wallaby.pouch <wallaby.pouch@91dd7c56-102c-11df-9f54-17e475596acb>
2013-04-29 20:09:25 +0000

    Small improvements to memory usage during parsing

--- bashsupport/src/com/ansorgit/plugins/bash/lang/psi/impl/BashBaseElementImpl.java
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/psi/impl/BashBaseElementImpl.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
- * Copyright 2011 Joachim Ansorg, mail@ansorg-it.com
- * File: BashPsiElementImpl.java, Class: BashPsiElementImpl
- * Last modified: 2011-04-30 16:33
+/*
+ * Copyright 2013 Joachim Ansorg, mail@ansorg-it.com
+ * File: BashBaseElementImpl.java, Class: BashBaseElementImpl
+ * Last modified: 2013-04-20
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package com.ansorgit.plugins.bash.lang.psi.impl;
 
@@ -51,7 +51,7 @@ import java.util.Set;
  *
  * @author Joachim Ansorg
  */
-public abstract class BashBaseElementImpl<T extends StubElement> extends StubBasedPsiElementBase<T>  implements BashPsiElement {
+public abstract class BashBaseElementImpl<T extends StubElement> extends StubBasedPsiElementBase<T> implements BashPsiElement {
     private final String name;
 
     public BashBaseElementImpl(final ASTNode astNode) {
