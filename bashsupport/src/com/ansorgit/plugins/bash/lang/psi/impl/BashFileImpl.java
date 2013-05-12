02aa1cf00afa26f3265cd961ef6955f950f51d79
J. Ansorg <github@joachim-ansorg.de>
2013-05-12 20:10:32 +0000

    fixed some issues / exceptions after renaming files

--- bashsupport/src/com/ansorgit/plugins/bash/lang/psi/impl/BashFileImpl.java
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/psi/impl/BashFileImpl.java
@@ -1,20 +1,20 @@
-/*******************************************************************************
- * Copyright 2011 Joachim Ansorg, mail@ansorg-it.com
+/*
+ * Copyright 2013 Joachim Ansorg, mail@ansorg-it.com
  * File: BashFileImpl.java, Class: BashFileImpl
- * Last modified: 2011-04-30 16:33
+ * Last modified: 2013-05-12
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *      http://www.apache.org/licenses/LICENSE-2.0
+ *    http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- ******************************************************************************/
+ */
 
 package com.ansorgit.plugins.bash.lang.psi.impl;
 
@@ -22,31 +22,21 @@ import com.ansorgit.plugins.bash.file.BashFileType;
 import com.ansorgit.plugins.bash.lang.psi.BashVisitor;
 import com.ansorgit.plugins.bash.lang.psi.api.BashFile;
 import com.ansorgit.plugins.bash.lang.psi.api.BashShebang;
-import com.ansorgit.plugins.bash.lang.psi.api.command.BashIncludeCommand;
 import com.ansorgit.plugins.bash.lang.psi.api.function.BashFunctionDef;
-import com.ansorgit.plugins.bash.lang.psi.stubs.index.BashIncludeCommandIndex;
 import com.ansorgit.plugins.bash.lang.psi.util.BashPsiUtils;
-import com.google.common.collect.Lists;
 import com.intellij.extapi.psi.PsiFileBase;
 import com.intellij.openapi.fileTypes.FileType;
-import com.intellij.psi.*;
+import com.intellij.psi.FileViewProvider;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.PsiElementVisitor;
+import com.intellij.psi.ResolveState;
 import com.intellij.psi.scope.PsiScopeProcessor;
-import com.intellij.psi.search.GlobalSearchScope;
-import com.intellij.psi.stubs.StubIndex;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
 
 /**
  * PSI implementation for a Bash file.
  */
 public class BashFileImpl extends PsiFileBase implements BashFile {
-    //we cache the include commands itself to avoid broken state if currently missing file is
-    //added later on
-    private List<BashIncludeCommand> includeCommands;
-
     public BashFileImpl(FileViewProvider viewProvider) {
         super(viewProvider, BashFileType.BASH_LANGUAGE);
     }
@@ -62,44 +52,6 @@ public class BashFileImpl extends PsiFileBase implements BashFile {
 
     public BashFunctionDef[] functionDefinitions() {
         return findChildrenByClass(BashFunctionDef.class);
-    }
-
-    /**
-     * Returns the included files and all included subfiles.
-     *
-     * @param result   The holder of the found included files
-     * @param diveDeep
-     * @param bashOnly
-     * @return
-     */
-    private void findIncludedFiles(final Set<PsiFile> result, final boolean diveDeep, final boolean bashOnly) {
-        //the unfiltered list of included files is cached
-        //because it is expensive to compute the list every time
-
-        if (includeCommands == null) {
-            final List<BashIncludeCommand> commands = Lists.newLinkedList();
-
-            PsiFile file = getContainingFile();
-            Collection<BashIncludeCommand> indexedCommands = StubIndex.getInstance().get(BashIncludeCommandIndex.KEY, file.getName(), getProject(), GlobalSearchScope.allScope(getProject()));
-
-            commands.addAll(indexedCommands);
-
-            includeCommands = commands;
-        }
-
-        for (BashIncludeCommand includeCommand : includeCommands) {
-            PsiFile file = includeCommand.getFileReference().findReferencedFile();
-
-            if (result.contains(file) || bashOnly && !(file instanceof BashFile)) {
-                continue;
-            }
-
-            result.add(file);
-
-            if (diveDeep && file instanceof BashFileImpl) {
-                ((BashFileImpl) file).findIncludedFiles(result, true, bashOnly);
-            }
-        }
     }
 
     @Override
@@ -118,7 +70,6 @@ public class BashFileImpl extends PsiFileBase implements BashFile {
 
     @Override
     public void subtreeChanged() {
-        this.includeCommands = null;
 
         super.subtreeChanged();
     }
