c348705d6b3511c273ee3601428d35913d3e06b3
J. Ansorg <github@joachim-ansorg.de>
2014-11-25 23:46:56 +0100

    #166: Support for ToDo-Strings in Bash comments

---
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/psi/BashFilterLexer.java
@@ -0,0 +1,32 @@
+package com.ansorgit.plugins.bash.lang.psi;
+
+import com.ansorgit.plugins.bash.lang.lexer.BashTokenTypes;
+import com.intellij.lexer.Lexer;
+import com.intellij.psi.impl.cache.impl.BaseFilterLexer;
+import com.intellij.psi.impl.cache.impl.OccurrenceConsumer;
+import com.intellij.psi.search.UsageSearchContext;
+import com.intellij.psi.tree.IElementType;
+
+public class BashFilterLexer extends BaseFilterLexer {
+
+    protected BashFilterLexer(Lexer lexer, OccurrenceConsumer consumer) {
+        super(lexer, consumer);
+    }
+
+    @Override
+    public void advance() {
+        IElementType tokenType = myDelegate.getTokenType();
+
+        if (tokenType == BashTokenTypes.COMMENT) {
+            scanWordsInToken(UsageSearchContext.IN_COMMENTS, false, false);
+            advanceTodoItemCountsInToken();
+        }
+        /*else if (tokenType == BashTokenTypes.STRING2) {
+            scanWordsInToken(UsageSearchContext.IN_STRINGS, false, false);
+        } else {
+            scanWordsInToken(UsageSearchContext.IN_PLAIN_TEXT, false, false);
+        } */
+
+        myDelegate.advance();
+    }
+}
