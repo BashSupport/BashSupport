afb42d937e330fc46340e6077f97894699952182
wallaby.pouch <j5k8g8t3>
2015-01-28 23:06:32 +0100

    inspection refactoring in progress

--- bashsupport/src/com/ansorgit/plugins/bash/lang/psi/stubs/index/BashIndexVersion.java
+++ bashsupport/src/com/ansorgit/plugins/bash/lang/psi/stubs/index/BashIndexVersion.java
@@ -4,9 +4,11 @@ package com.ansorgit.plugins.bash.lang.psi.stubs.index;
  * Configures the versions of the available Bash indexes.
  */
 public final class BashIndexVersion {
+    public static final int CACHES_VERSION = 1;
+
     private BashIndexVersion() {
     }
 
-    public static int VERSION = 10;
-    public static int ID_VERSION = 2;
+    public static final int VERSION = 11;
+    public static final int ID_VERSION = 3;
 }
