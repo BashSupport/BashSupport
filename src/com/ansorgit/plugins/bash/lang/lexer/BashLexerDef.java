0fc250d545f5fdeb99cfa02021c47c7b62f33c2c
wallaby.pouch <j5k8g8t3>
2015-05-12 20:15:07 +0200

    Moved code from bash.flex into an interface and implementation class

---
+++ src/com/ansorgit/plugins/bash/lang/lexer/BashLexerDef.java
@@ -0,0 +1,57 @@
+package com.ansorgit.plugins.bash.lang.lexer;
+
+import com.intellij.lexer.FlexLexer;
+
+/**
+ */
+public interface BashLexerDef extends BashTokenTypes, FlexLexer {
+    boolean isBash4();
+
+    /**
+     * Goes to the given state and stores the previous state on the stack of states.
+     * This makes it possible to have several levels of lexing, e.g. for $(( 1+ $(echo 3) )).
+     */
+    void goToState(int newState);
+
+    /**
+     * Goes back to the previous state of the lexer. If there
+     * is no previous state then YYINITIAL, the initial state, is chosen.
+     */
+    void backToPreviousState();
+
+    int openParenthesisCount();
+
+    void incOpenParenthesisCount();
+
+    void decOpenParenthesisCount();
+
+    boolean isParamExpansionHash();
+
+    void setParamExpansionHash(boolean paremeterExpansionHash);
+
+    boolean isParamExpansionWord();
+
+    void setParamExpansionWord(boolean paremeterExpansionWord);
+
+    boolean isParamExpansionOther();
+
+    void setParamExpansionOther(boolean paremeterExpansionOther);
+
+    boolean isInCaseBody();
+
+    void setInCaseBody(boolean inCaseBody);
+
+    StringParsingState stringParsingState();
+
+    boolean isEmptyConditionalCommand();
+
+    void setEmptyConditionalCommand(boolean emptyConditionalCommand);
+
+    boolean isExpectArithExpression();
+
+    void setExpectArithExpression(boolean expectArithExpression);
+
+    boolean isStartNewArithExpression();
+
+    void setStartNewArithExpression(boolean startNewArithExpression);
+}
