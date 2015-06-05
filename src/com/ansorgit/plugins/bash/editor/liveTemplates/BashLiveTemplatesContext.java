33acda094914601daa8b8a97e313e2ac0e0ca885
wallaby.pouch <j5k8g8t3>
2015-06-05 10:35:30 +0200

    No liveTemplate suggestions whille typing in a Bash comment

--- src/com/ansorgit/plugins/bash/editor/liveTemplates/BashLiveTemplatesContext.java
+++ src/com/ansorgit/plugins/bash/editor/liveTemplates/BashLiveTemplatesContext.java
@@ -1,8 +1,13 @@
 package com.ansorgit.plugins.bash.editor.liveTemplates;
 
-import com.ansorgit.plugins.bash.lang.psi.api.BashFile;
+import com.ansorgit.plugins.bash.file.BashFileType;
 import com.intellij.codeInsight.template.TemplateContextType;
+import com.intellij.lang.Language;
+import com.intellij.psi.PsiComment;
+import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.util.PsiTreeUtil;
+import com.intellij.psi.util.PsiUtilCore;
 import org.jetbrains.annotations.NotNull;
 
 /**
@@ -15,6 +20,18 @@ public class BashLiveTemplatesContext extends TemplateContextType {
 
     @Override
     public boolean isInContext(@NotNull PsiFile file, int offset) {
-        return file instanceof BashFile;
+        Language language = PsiUtilCore.getLanguageAtOffset(file, offset);
+        if (language.isKindOf(BashFileType.BASH_LANGUAGE)) {
+            PsiElement element = file.findElementAt(offset);
+            if (element == null) {
+                //if a user edits at the end of a comment at the end of a file then findElementAt returns null
+                //(for yet unknown reasons)
+                element = file.findElementAt(offset - 1);
+            }
+
+            return PsiTreeUtil.getParentOfType(element, PsiComment.class, false) == null;
+        }
+
+        return false;
     }
 }
--- src/com/ansorgit/plugins/bash/editor/liveTemplates/BashLiveTemplatesProvider.java
+++ src/com/ansorgit/plugins/bash/editor/liveTemplates/BashLiveTemplatesProvider.java
@@ -6,13 +6,13 @@ import org.jetbrains.annotations.Nullable;
 /**
  * Live templates for the Bash language.
  */
-public class LiveTemplatesProvider implements DefaultLiveTemplatesProvider {
+public class BashLiveTemplatesProvider implements DefaultLiveTemplatesProvider {
 
     private static final String[] EMPTY = new String[0];
 
     @Override
     public String[] getDefaultLiveTemplateFiles() {
-        return new String[]{"liveTemplates/Bash"};
+        return new String[]{"/liveTemplates/Bash"};
     }
 
     @Nullable
