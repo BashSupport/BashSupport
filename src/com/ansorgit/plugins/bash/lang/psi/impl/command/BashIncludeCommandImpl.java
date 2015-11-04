73744125a3d9f9ea308f05c3c0cd18ad5cf71640
wallaby.pouch <j5k8g8t3>
2015-11-04 22:35:44 +0100

    Work in progress to fix internal command documentation lookup

--- src/com/ansorgit/plugins/bash/lang/psi/impl/command/BashIncludeCommandImpl.java
+++ src/com/ansorgit/plugins/bash/lang/psi/impl/command/BashIncludeCommandImpl.java
@@ -30,6 +30,7 @@ import com.google.common.collect.Maps;
 import com.google.common.collect.Multimap;
 import com.google.common.collect.Multimaps;
 import com.intellij.lang.ASTNode;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.psi.*;
 import com.intellij.psi.scope.PsiScopeProcessor;
@@ -57,7 +58,13 @@ public class BashIncludeCommandImpl extends AbstractBashCommand<BashIncludeComma
     @Nullable
     @Override
     public PsiReference getReference() {
-        return null;
+        PsiElement element = commandElement();
+        if (element == null) {
+            return null;
+        }
+
+        TextRange range = TextRange.from(0, element.getTextLength()).shiftRight(element.getStartOffsetInParent());
+        return new PsiReferenceBase.Immediate<PsiElement>(this, range, true, this);
     }
 
     @Nullable
