d29dc771aec3316527eef265cdd70504a3844a59
wallaby.pouch <j5k8g8t3>
2015-11-05 00:06:40 +0100

    Fixed bad merge

--- src/com/ansorgit/plugins/bash/runner/repl/BashConsoleRunner.java
+++ src/com/ansorgit/plugins/bash/runner/repl/BashConsoleRunner.java
@@ -2,13 +2,13 @@
  * Copyright 2011 Joachim Ansorg, mail@ansorg-it.com
  * File: BashConsoleRunner.java, Class: BashConsoleRunner
  * Last modified: 2011-05-17 22:20
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -23,11 +23,11 @@ import com.ansorgit.plugins.bash.lang.psi.api.BashFile;
 import com.ansorgit.plugins.bash.util.BashInterpreterDetection;
 import com.intellij.execution.ExecutionException;
 import com.intellij.execution.configurations.GeneralCommandLine;
-import com.intellij.execution.console.LanguageConsoleImpl;
-import com.intellij.execution.console.ProcessBackedConsoleExecuteActionHandler;
+import com.intellij.execution.console.LanguageConsoleViewImpl;
 import com.intellij.execution.process.ColoredProcessHandler;
 import com.intellij.execution.process.OSProcessHandler;
 import com.intellij.execution.runners.AbstractConsoleRunnerWithHistory;
+import com.intellij.execution.runners.ConsoleExecuteActionHandler;
 import com.intellij.openapi.project.Project;
 import org.jetbrains.annotations.NotNull;
 
@@ -38,15 +38,15 @@ import org.jetbrains.annotations.NotNull;
  * Date: 13.05.2010
  * Time: 00:45:23
  */
-public class BashConsoleRunner extends AbstractConsoleRunnerWithHistory<LanguageConsoleImpl> {
+public class BashConsoleRunner extends AbstractConsoleRunnerWithHistory<LanguageConsoleViewImpl> {
     public BashConsoleRunner(Project myProject, String workingDir) {
         super(myProject, "Bash", workingDir);
     }
 
     @Override
-    protected LanguageConsoleImpl createConsoleView() {
-        LanguageConsoleImpl consoleView = new LanguageConsoleImpl(getProject(), "Bash", BashFileType.BASH_LANGUAGE);
-        consoleView.getFile().putUserData(BashFile.LANGUAGE_CONSOLE_MARKER, true);
+    protected LanguageConsoleViewImpl createConsoleView() {
+        LanguageConsoleViewImpl consoleView = new LanguageConsoleViewImpl(getProject(), "Bash", BashFileType.BASH_LANGUAGE);
+        consoleView.getConsole().getFile().putUserData(BashFile.LANGUAGE_CONSOLE_MARKER, true);
 
         return consoleView;
     }
@@ -72,7 +72,7 @@ public class BashConsoleRunner extends AbstractConsoleRunnerWithHistory<Language
 
     @NotNull
     @Override
-    protected ProcessBackedConsoleExecuteActionHandler createExecuteActionHandler() {
-        return new ProcessBackedConsoleExecuteActionHandler(getProcessHandler(), true);
+    protected ConsoleExecuteActionHandler createConsoleExecuteActionHandler() {
+        return new ConsoleExecuteActionHandler(getProcessHandler(), true);
     }
 }
