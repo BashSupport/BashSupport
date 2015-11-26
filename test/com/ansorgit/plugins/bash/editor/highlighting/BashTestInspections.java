22130bb419886598579d66baa9a751de18b930dc
wallaby.pouch <j5k8g8t3>
2015-11-26 22:16:36 +0100

    Rolled inspections into one to avoid duplicate resolve calls

--- test/com/ansorgit/plugins/bash/editor/highlighting/BashTestInspections.java
+++ test/com/ansorgit/plugins/bash/editor/highlighting/BashTestInspections.java
@@ -30,9 +30,7 @@ public class BashTestInspections implements InspectionToolProvider {
                 com.ansorgit.plugins.bash.editor.inspections.inspections.ConvertBackquoteInspection.class,
                 com.ansorgit.plugins.bash.editor.inspections.inspections.UnusedFunctionDefInspection.class,
                 com.ansorgit.plugins.bash.editor.inspections.inspections.SimpleArrayUseInspection.class,
-                com.ansorgit.plugins.bash.editor.inspections.inspections.ArrayUseOfSimpleVarInspection.class,
                 com.ansorgit.plugins.bash.editor.inspections.inspections.InternalCommandFunctionOverrideInspection.class
-
         };
     }
 }
