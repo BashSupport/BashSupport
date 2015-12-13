c101be22a547ef82695d4e3388ececbdae871b94
wallaby.pouch <j5k8g8t3>
2015-12-13 20:51:31 +0100

    Fixed compile error

--- src/com/ansorgit/plugins/bash/lang/lexer/BashTokenTypes.java
+++ src/com/ansorgit/plugins/bash/lang/lexer/BashTokenTypes.java
@@ -18,7 +18,6 @@
 
 package com.ansorgit.plugins.bash.lang.lexer;
 
-import com.ansorgit.plugins.bash.lang.parser.eval.BashEvalElementType;
 import com.intellij.psi.TokenType;
 import com.intellij.psi.tree.IElementType;
 import com.intellij.psi.tree.TokenSet;
