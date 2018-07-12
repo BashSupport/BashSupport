12f8fda38e2c8952a9c697ad0d119f6ff42c3c40
Daniel Heid <Daniel.Heid@parshipelite.com>
2018-07-12 17:13:58 +0200

    #556 Add inspection for function names that should be lower case snake case

---
+++ testData/psi/inspection/functionNamesShouldBeLowerSnakeCaseInspection/ok/src/A.bash
@@ -0,0 +1,37 @@
+#!/bin/sh
+
+function this_is_a_long_function_name() {
+  echo "Hello world"
+}
+
+function thisisalongfunctionname() {
+  echo "Hello world"
+}
+
+function package::thisisalongfunctionname() {
+  echo "Hello world"
+}
+
+this_is_a_long_function_name2() {
+  echo "Hello world"
+}
+
+thisisalongfunctionname2() {
+  echo "Hello world"
+}
+
+package::thisisalongfunctionname2() {
+  echo "Hello world"
+}
+
+function this_is_a_long_function_name3 {
+  echo "Hello world"
+}
+
+function thisisalongfunctionname3 {
+  echo "Hello world"
+}
+
+function package::thisisalongfunctionname3 {
+  echo "Hello world"
+}
