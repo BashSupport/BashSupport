bebdfb71bf6a619ec4ddae8e9b22323de0e29b4b
jansorg <github@joachim-ansorg.de>
2018-08-25 14:04:11 +0200

    the default nesting level is 1, so there shouldn't be a replacement of $NSNAM

--- testData/folding/VariableFoldingWithCondition.bash
+++ testData/folding/VariableFoldingWithCondition.bash
@@ -6,25 +6,22 @@ NSNAM="nsnam-www.coe-hosted.gatech.edu"
 HOST="bla-bla-bla"
 if [ $nsnam -eq 1 ]; then
     HOST=<fold text='nsnam-www.coe-hosted.gatech.edu'>$NSNAM</fold>
-    say "-n forcing HOST = <fold text='nsnam-www.coe-hosted.gatech.edu'>$HOST</fold>"
+    say "-n forcing HOST = <fold text='$NSNAM'>$HOST</fold>"
 fi
 
-#!/usr/bin/env bash
 A="value of A"
 if true; then
     A="conditional true of A"
 else
     A="conditional false of A"
 fi
-
 echo "hello $A, how are you?"
 
-
 daily=0
-
 while getopts :pndth option ; do
     case <fold text='option'>$option</fold> in
 	(d)  daily=1  ;;
+	esac
 done
 
 if [ $daily -eq 1 ] ; then
