10a0aaa01111d48d5461dd7396940aab1434203b
jansorg <github@joachim-ansorg.de>
2018-11-21 19:15:27 +0100

    make a subshell a valid block

--- src/com/ansorgit/plugins/bash/lang/psi/api/expression/BashSubshellCommand.java
+++ src/com/ansorgit/plugins/bash/lang/psi/api/expression/BashSubshellCommand.java
@@ -15,11 +15,12 @@
 
 package com.ansorgit.plugins.bash.lang.psi.api.expression;
 
+import com.ansorgit.plugins.bash.lang.psi.api.BashBlock;
 import com.ansorgit.plugins.bash.lang.psi.api.BashPsiElement;
 
 /**
  * @author jansorg
  */
-public interface BashSubshellCommand extends BashPsiElement {
+public interface BashSubshellCommand extends BashPsiElement, BashBlock {
     String getCommandText();
 }
