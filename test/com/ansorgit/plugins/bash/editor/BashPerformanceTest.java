93b62d33fbd1cce18a27d58adabe26f64175011d
jansorg <github@joachim-ansorg.de>
2019-02-18 18:50:59 +0100

    fix test

--- test/com/ansorgit/plugins/bash/editor/BashPerformanceTest.java
+++ test/com/ansorgit/plugins/bash/editor/BashPerformanceTest.java
@@ -32,35 +32,33 @@ import java.util.ArrayList;
  */
 public class BashPerformanceTest extends LightBashCodeInsightFixtureTestCase {
     public void testEditorPerformance() {
-        doTest(2);
+        doTest(10);
     }
 
     // editorPerformanceLarge: 14078% longer. Expected: 407ms. Actual: 57705ms (57.7s). Timings: CPU=47 (23% of the etalon), I/O=13 (13% of the etalon), total=60 (13% of the etalon) 12 cores.
     public void testEditorPerformanceLarge() {
-        //doTest(100);
+        doTest(100);
     }
 
     private void doTest(final int iterations) {
         myFixture.configureByFile("functions_issue96.bash");
+        enableInspections();
 
         long start = System.currentTimeMillis();
-        PlatformTestUtil.startPerformanceTest(getTestName(true), iterations * 500, new ThrowableRunnable() {
-            @Override
-            public void run() throws Throwable {
-                for (int i = 0; i < iterations; i++) {
-                    long innerStart = System.currentTimeMillis();
-                    Editor editor = myFixture.getEditor();
-                    editor.getCaretModel().moveToOffset(editor.getDocument().getTextLength());
+        PlatformTestUtil.startPerformanceTest(getTestName(true), iterations * 1000, () -> {
+            for (int i = 0; i < iterations; i++) {
+                long innerStart = System.currentTimeMillis();
+                Editor editor = myFixture.getEditor();
+                editor.getCaretModel().moveToOffset(editor.getDocument().getTextLength());
 
-                    myFixture.type("\n");
-                    myFixture.type("echo \"hello world\"\n");
-                    myFixture.type("pri");
-                    myFixture.complete(CompletionType.BASIC);
+                myFixture.type("\n");
+                myFixture.type("echo \"hello world\"\n");
+                myFixture.type("pri");
+                myFixture.complete(CompletionType.BASIC);
 
-                    System.out.println("Cycle duration: " + (System.currentTimeMillis() - innerStart));
-                }
+                System.out.println("Cycle duration: " + (System.currentTimeMillis() - innerStart));
             }
-        }).usesAllCPUCores().attempts(1).assertTiming();
+        }).cpuBound().attempts(3).assertTiming();
 
         System.out.println("Complete duration: " + (System.currentTimeMillis() - start));
     }
