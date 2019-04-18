8518630482180d3e6721d97ce00ef068fbe9f96f
jansorg <github@joachim-ansorg.de>
2019-04-18 17:37:29 +0200

    Insert line continuation character only when line wrapping is active or when the user pressed enter in string content or variable inside of a string literal

--- src/com/ansorgit/plugins/bash/editor/EnterInStringLiteralHandler.java
+++ src/com/ansorgit/plugins/bash/editor/EnterInStringLiteralHandler.java
@@ -15,8 +15,13 @@
 
 package com.ansorgit.plugins.bash.editor;
 
+import com.ansorgit.plugins.bash.lang.lexer.BashTokenTypes;
 import com.ansorgit.plugins.bash.lang.psi.api.BashFile;
+import com.ansorgit.plugins.bash.lang.psi.api.BashString;
+import com.ansorgit.plugins.bash.lang.psi.api.vars.BashVar;
+import com.intellij.codeInsight.editorActions.AutoHardWrapHandler;
 import com.intellij.codeInsight.editorActions.enter.EnterHandlerDelegateAdapter;
+import com.intellij.ide.DataManager;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorModificationUtil;
@@ -25,30 +30,53 @@ import com.intellij.openapi.util.Ref;
 import com.intellij.psi.PsiDocumentManager;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiFile;
+import com.intellij.psi.impl.source.tree.LeafPsiElement;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 /**
- * This enter handler inserts a line continuation character
+ * This enter handler inserts a line continuation character.
+ * User typing: only inserted in a string literal
+ * On line wrapping: Always inserted
+ *
  * @author jansorg
  */
 public class EnterInStringLiteralHandler extends EnterHandlerDelegateAdapter {
     @Override
     public Result preprocessEnter(@NotNull PsiFile file, @NotNull Editor editor, @NotNull Ref<Integer> caretOffset, @NotNull Ref<Integer> caretAdvance, @NotNull DataContext dataContext, @Nullable EditorActionHandler originalHandler) {
-        if (file instanceof BashFile) {
-            int offset = caretOffset.get();
+        if (!(file instanceof BashFile)) {
+            return Result.Continue;
+        }
 
-            PsiElement psi = file.findElementAt(offset);
-            if (psi != null && offset >= psi.getTextOffset()) {
-                EditorModificationUtil.insertStringAtCaret(editor, "\\\n");
+        int offset = caretOffset.get();
+        PsiElement psi = file.findElementAt(offset);
+        if (psi == null || psi.getNode().getElementType() == BashTokenTypes.LINE_FEED) {
+            // do not add a line continuation at end of line
+            return Result.Continue;
+        }
 
-                PsiDocumentManager mgr = PsiDocumentManager.getInstance(file.getProject());
-                mgr.commitDocument(editor.getDocument());
+        boolean isWrapping = Boolean.TRUE.equals(DataManager.getInstance().loadFromDataContext(dataContext, AutoHardWrapHandler.AUTO_WRAP_LINE_IN_PROGRESS_KEY));
+        boolean inString = findWrappingContext(psi) instanceof BashString;
+        if (!inString && !isWrapping) {
+            return Result.Continue;
+        }
 
-                return Result.Stop;
-            }
+        if (offset >= psi.getTextOffset() && psi.getNode().getElementType() != BashTokenTypes.LINE_FEED) {
+            EditorModificationUtil.insertStringAtCaret(editor, "\\\n");
+
+            PsiDocumentManager mgr = PsiDocumentManager.getInstance(file.getProject());
+            mgr.commitDocument(editor.getDocument());
+
+            return Result.Stop;
         }
 
         return Result.Continue;
+    }
+
+    private PsiElement findWrappingContext(PsiElement start) {
+        if (start instanceof LeafPsiElement || start instanceof BashVar) {
+            return findWrappingContext(start.getParent());
+        }
+        return start;
     }
 }
--- test/com/ansorgit/plugins/bash/editor/EnterInStringLiteralHandlerTest.java
+++ test/com/ansorgit/plugins/bash/editor/EnterInStringLiteralHandlerTest.java
@@ -21,29 +21,32 @@ import com.intellij.openapi.util.text.StringUtil;
 import com.intellij.psi.codeStyle.CodeStyleSettingsManager;
 import com.intellij.psi.codeStyle.CommonCodeStyleSettings;
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * @author jansorg
  */
 public class EnterInStringLiteralHandlerTest extends LightBashCodeInsightFixtureTestCase {
+    @Before
+    protected void setUp() throws Exception {
+        super.setUp();
+        CodeStyleSettingsManager.getInstance(getProject()).getTemporarySettings().WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
+    }
+
     @Test
     public void testEnterInCommand() {
-        CodeStyleSettingsManager.getInstance(getProject()).getTemporarySettings().WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
-
         // 125 chars, longer than the default right margin
         myFixture.configureByText(BashFileType.BASH_FILE_TYPE, StringUtil.repeat("a", 125));
         myFixture.getEditor().getCaretModel().moveToOffset(121);
         myFixture.type('\n');
 
-        Assert.assertEquals("Expected that line continuation was inserted",
-                StringUtil.repeat("a", 121) + "\\\naaaa", myFixture.getEditor().getDocument().getText());
+        Assert.assertEquals("Expected that no line continuation was inserted on enter outside of a string literal",
+                StringUtil.repeat("a", 121) + "\naaaa", myFixture.getEditor().getDocument().getText());
     }
 
     @Test
     public void testEnterInStringLiteratl() {
-        CodeStyleSettingsManager.getInstance(getProject()).getTemporarySettings().WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
-
         // 125 chars, longer than the default right margin
         myFixture.configureByText(BashFileType.BASH_FILE_TYPE, "\"" + StringUtil.repeat("a", 125) + "\"");
         myFixture.getEditor().getCaretModel().moveToOffset(122); // opening quote + 121
@@ -55,8 +58,6 @@ public class EnterInStringLiteralHandlerTest extends LightBashCodeInsightFixture
 
     @Test
     public void testEnterAtEOLString() {
-        CodeStyleSettingsManager.getInstance(getProject()).getTemporarySettings().WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
-
         // 125 chars, longer than the default right margin
         String content = "\"" + StringUtil.repeat("a", 125) + "\"";
         myFixture.configureByText(BashFileType.BASH_FILE_TYPE, content);
@@ -69,15 +70,45 @@ public class EnterInStringLiteralHandlerTest extends LightBashCodeInsightFixture
 
     @Test
     public void testEnterAtEOLCommand() {
-        CodeStyleSettingsManager.getInstance(getProject()).getTemporarySettings().WRAP_ON_TYPING = CommonCodeStyleSettings.WrapOnTyping.WRAP.intValue;
+        // 125 chars, longer than the default right margin
+        String content = StringUtil.repeat("a", 125) + "\n";
+        myFixture.configureByText(BashFileType.BASH_FILE_TYPE, content);
+        myFixture.getEditor().getCaretModel().moveToOffset(content.length() - 1);
+        myFixture.type('\n');
 
+        Assert.assertEquals("Expected that line continuation isn't inserted at the end of a line",
+                content + "\n", myFixture.getEditor().getDocument().getText());
+    }
+
+    @Test
+    public void testEnterAtEOF() {
         // 125 chars, longer than the default right margin
         String content = StringUtil.repeat("a", 125);
         myFixture.configureByText(BashFileType.BASH_FILE_TYPE, content);
         myFixture.getEditor().getCaretModel().moveToOffset(content.length());
         myFixture.type('\n');
 
-        Assert.assertEquals("Expected that line continuation isn't inserted at the end of a line",
+        Assert.assertEquals("Expected that line continuation isn't inserted at the end of a file",
                 content + "\n", myFixture.getEditor().getDocument().getText());
+    }
+
+    @Test
+    public void testEnterInSubshell() {
+        // 125 chars, longer than the default right margin
+        myFixture.configureByText(BashFileType.BASH_FILE_TYPE, "\"hello $(echo <caret>world)\"");
+        myFixture.type('\n');
+
+        Assert.assertEquals("No line continuation must be inserted in a shell embedded in a string",
+                "\"hello $(echo \nworld)\"", myFixture.getEditor().getDocument().getText());
+    }
+
+    @Test
+    public void testEnterInVar() {
+        // 125 chars, longer than the default right margin
+        myFixture.configureByText(BashFileType.BASH_FILE_TYPE, "\"hello $<caret>var\"");
+        myFixture.type('\n');
+
+        Assert.assertEquals("Line continuation must be inserted in a variable embedded in a string",
+                "\"hello $\\\nvar\"", myFixture.getEditor().getDocument().getText());
     }
 }
