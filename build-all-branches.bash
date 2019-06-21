7010c7bb75947d6ea5d5e23eec769669d1f77d42
J. Ansorg <github@joachim-ansorg.de>
2019-06-21 10:53:57 +0200

    #656: Implement indexPatternBuilder to enable multiline fixme comments (#715)
    * #656: Implement indexPatternBuilder to enable multiline fixme comments in Bash files
    
    * Move tests which needs IJ >= 2018.3 into a separate test source dir "test-183".
    Modify build-all-branches.bash to make use of this setup.
    
    * enable the test, disable the test-183 in IJ sourcesets as it's still using 145.x as default SDK
    
    * remove empty constructor

--- build-all-branches.bash
+++ build-all-branches.bash
@@ -1,9 +1,28 @@
 #!/usr/bin/env bash
 test -f "$HOME/.jdk8" && . $HOME/.jdk8
 
+function ideaBranch {
+    if [[ "$1" == 191.* || "$1" == 2019.1.* ]]; then
+        echo "191"
+    elif [[ "$1" == 183.* || "$1" == 2018.3.* ]]; then
+        echo "183"
+    elif [[ "$1" == 182.* || "$1" == 2018.2.* ]]; then
+        echo "182"
+    elif [[ "$1" == 181.* || "$1" == 2018.1.* ]]; then
+        echo "181"
+    elif [[ "$1" == 173.* || "$1" == 2017.3.* ]]; then
+        echo "173"
+    elif [[ "$1" == 172.* || "$1" == 2017.2.* ]]; then
+        echo "172"
+    elif [[ "$1" == 171.* || "$1" == 2017.1.* ]]; then
+        echo "171"
+    fi
+}
+
 for v in "191.5701.16" "183.4139.22" "2018.2.3" "2018.1.6" "2017.3.5" "2017.2.6" "2017.1.6" "2016.3.8" "2016.2.5" "2016.1.4"; do
-    echo "## Building with version $v..."
-    _JAVA_OPTIONS="" JAVA_OPTS="" gradle -Dbash.skipUrls="true" -PideaVersion="$v" -PideaBranch="" clean build
+    branch="$(ideaBranch $v)"
+    echo "## Building with version $v, IDEA branch $branch..."
+    _JAVA_OPTIONS="" JAVA_OPTS="" gradle -Dbash.skipUrls="true" -PideaVersion="$v" -PideaBranch="$branch" clean build
 
     status=$?
     if [[ $status -ne 0 ]]; then
