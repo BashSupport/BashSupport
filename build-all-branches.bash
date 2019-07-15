785133a9cadf7434a389f2049c21d5e417aa3231
jansorg <github@joachim-ansorg.de>
2019-07-15 11:59:23 +0200

    Use latest EAP in build-all-branches.bash

--- build-all-branches.bash
+++ build-all-branches.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 test -f "$HOME/.jdk8" && . $HOME/.jdk8
 
-for v in "192.5587.17-EAP-SNAPSHOT"; do
+for v in "192.5728.12-EAP-SNAPSHOT"; do
     echo "## Building with version $v..."
-    _JAVA_OPTIONS="" JAVA_OPTS="" gradle -Dbash.skipUrls="true" -PideaVersion="$v" -PideaBranch="" clean build
+    _JAVA_OPTIONS="" JAVA_OPTS="" gradle -Dbash.skipUrls="true" -PideaVersion="$v" -PideaBranch="192" clean build
 
     status=$?
     if [[ $status -ne 0 ]]; then
